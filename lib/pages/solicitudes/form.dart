a11141a9dde6189b91e24938fd2c968a999d6c0d
Oscar Miguel Toro Torres <omigueltoro@gmail.com>
2021-05-06 23:09:01 -0500

    Se implementa estructura base de paginas de la aplicación

---
+++ lib/pages/solicitudes/form.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FormPage extends StatelessWidget {
+  final List<Text> solicitud;
+
+  FormPage({@required this.solicitud});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: null,
+    );
+  }
+}
---
+++ lib/pages/solicitudes/table.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TablePage extends StatefulWidget {
+  @override
+  _TablePageState createState() => _TablePageState();
+}
+
+class _TablePageState extends State<TablePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Table();
+  }
+}
